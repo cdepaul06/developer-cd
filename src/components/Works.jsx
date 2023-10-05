638535f0c2255c18c8ca4f53817ffbc477cf8e28
Chris <73894631+cdepaul06@users.noreply.github.com>
2023-10-05 17:02:57 -0400

    final final fix for mobile

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,10 +17,58 @@ const ProjectCard = ({
 }) => {
   const isMobile = window.innerWidth < 768;
 
+  const cardContent = (
+    <>
+      <div className='relative w-full h-[230px]'>
+        <img
+          src={image}
+          alt='project_image'
+          className='w-full h-full object-cover rounded-2xl'
+        />
+        {source_code_link && (
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className='mt-5'>
+        <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+      </div>
+
+      <div className='mt-4 flex flex-wrap gap-2'>
+        {tags.map((tag) => (
+          <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+
+  if (isMobile) {
+    return (
+      <motion.div
+        variants={fadeInMobile(0, 1)}
+        className='bg-tertiary p-5 rounded-2xl w-full'
+      >
+        {cardContent}
+      </motion.div>
+    );
+  }
+
   return (
-    <motion.div
-      variants={!isMobile ? fadeIn("up", "spring", index * 0.5, 0.75) : ""}
-    >
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
@@ -29,44 +77,7 @@ const ProjectCard = ({
         }}
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
-        <div className='relative w-full h-[230px]'>
-          <img
-            src={image}
-            alt='project_image'
-            className='w-full h-full object-cover rounded-2xl'
-          />
-
-          {source_code_link && (
-            <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
-              >
-                <img
-                  src={github}
-                  alt='source code'
-                  className='w-1/2 h-1/2 object-contain'
-                />
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
-
-        <div className='mt-4 flex flex-wrap gap-2'>
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
+        {cardContent}
       </Tilt>
     </motion.div>
   );
